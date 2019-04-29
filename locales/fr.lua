84f23f40c1de9a7a94e0f2459fe89dc3552eeca7
Zerefdev <25670700+Zerefdev@users.noreply.github.com>
2019-04-29 12:46:23 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,12 @@
+Locales["fr"] = {
+  ["trunk_closed"] = "Ce coffre est ~r~fermé~s~.",
+  ["no_veh_nearby"] = "Il n'y a pas de véhicule près de vous.",
+  ["trunk_in_use"] = "Ce coffre est ~g~en cours~s~ d'utilisation.",
+  ["trunk_full"] = "Ce coffre est ~r~plein~s~.",
+  ["invalid_quantity"] = "Quantité ~r~invalide~s~",
+  ["cant_carry_more"] = "Vous ne pouvez plus en porter.",
+  ["nacho_veh"] = "Ce n'est pas votre véhicule.",
+  ["invalid_amount"] = "Montant ~r~invalide~s~",
+  ["insufficient_space"] = "Espace ~r~insuffisant~s~",
+  ["trunk_info"] = "<h3>Coffre de véhicule</h3><br><strong>Plaque:</strong> %s<br><strong>Capacité:</strong> %s / %s"
+}
