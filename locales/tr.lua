aa6429635a84a832e6e62182096c40f3419ff917
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-13 11:42:18 +0300

    Create tr.lua
    added tr.lang

---
+++ locales/tr.lua
@@ -0,0 +1,13 @@
+Locales["tr"] = {
+  ["trunk_closed"] = "Bagaj kapalı.",
+  ["no_veh_nearby"] = "Yakınlarda araç yok.",
+  ["trunk_in_use"] = "Bu Bagaj kullanımda.",
+  ["trunk_full"] = "Bu Bagaj dolu.",
+  ["invalid_quantity"] = "Geçersiz miktar",
+  ["cant_carry_more"] = "Artık taşıyamazsın.",
+  ["nacho_veh"] = "Bu senin aracın değil.",
+  ["invalid_amount"] = "Geçersiz tutar",
+  ["insufficient_space"] = "Yetersiz alan",
+  ["trunk_info"] = "<h3>Araç Bagajı</h3><br><strong>Plaka:</strong> %s<br><strong>Kapsite:</strong> %s / %s",
+  ["player_inv_no_space"] = "Envanterinizdeki bu öğe için yeterli alan yok!"
+}
